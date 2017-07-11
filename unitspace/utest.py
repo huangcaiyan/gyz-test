ef2a244df7e398160fdbee15ea07d70d856595e0
黄彩艳 <huangcaiyan@concordya.com>
2017-07-11 18:11:07 +0800

    test

---
+++ unitspace/utest.py
@@ -0,0 +1,36 @@
+import unittest
+from module import Calculator
+
+class ModuleTest(unittest.TestCase):
+
+    def setUp(self):
+        self.cal = Calculator(8,4)
+     
+    def tearDown(self):
+        pass
+        
+    
+    def test_add(self):
+        self.cal.add()
+        self.assertEqual(result,12)
+
+    def test_sub(self):
+        self.cal.sub()
+        self.assertEqual(result,4)
+
+    def test_mul(self):
+        self.cal.mul()
+        self.assertEqual(result,32)
+
+    def test_div(self):
+        self.cal.div()
+        self.assertEqual(result,2)
+
+    if __name__=="_main_":
+        # unittest.main()
+        suite = unittest.TestSuite()
+        suite.addTest(ModuleTest("test_add"))
+        suite.addTest(ModuleTest("test_sub"))
+
+        runner = unittest.TextTestRunner()
+        runner.run(suite)
